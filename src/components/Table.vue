128922bb9a15942a62df88d306463c5e877b68b0
Thong Bui <buiquangthong@gmail.com>
2019-07-26 13:35:10 +0700

    Update table component

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,12 +1,29 @@
 <template>
   <div class="table">
-    <b-table striped hover :items="items"></b-table>
+    <b-table striped hover show-empty
+      :items="data"
+      :fields="tableConfig.columns"
+      :sort-by.sync="tableConfig.sortBy"
+      :sort-desc.sync="tableConfig.sortDesc"
+      :current-page="tableConfig.currentPage"
+      :per-page="tableConfig.perPage">
+    </b-table>
+    <b-row>
+      <b-col>
+        <b-pagination
+          v-model="tableConfig.currentPage"
+          :total-rows="tableConfig.totalRows"
+          :per-page="tableConfig.perPage"
+          class="my-0"
+        ></b-pagination>
+      </b-col>
+    </b-row>
   </div>
 </template>
 
 <script>
   export default {
     id: 'table',
-    props: ['items']
+    props: ['data', 'tableConfig']
   }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home">
-    <Table v-bind:items="items" />
+    <b-card bg-variant="light" header="Table" class="text-left">
+      <Table v-bind:data="data" v-bind:tableConfig="tableConfig" />
+    </b-card>
   </div>
 </template>
 
@@ -11,13 +13,41 @@ export default {
   name: 'home',
   data: function () {
     return {
-      items: [
+      tableConfig: {
+        sortBy: 'age',
+        sortDesc: false,
+        currentPage: 1,
+        totalRows: 1,
+        perPage: 5,
+        columns: [
+          { key: 'last_name', sortable: true },
+          { key: 'first_name', sortable: true },
+          { key: 'age', sortable: true }
+        ]
+      },
+      data: [
+        { age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+        { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+        { age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+        { age: 38, first_name: 'Jami', last_name: 'Carney' },
+        { age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+        { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+        { age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+        { age: 38, first_name: 'Jami', last_name: 'Carney' },
+        { age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+        { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+        { age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+        { age: 38, first_name: 'Jami', last_name: 'Carney' },
         { age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
         { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
         { age: 89, first_name: 'Geneva', last_name: 'Wilson' },
         { age: 38, first_name: 'Jami', last_name: 'Carney' }
       ]
     }
+  },
+  mounted() {
+    // Initial number of rows
+    this.tableConfig.totalRows = this.data.length
   },
   components: {
     Table
